693e4dd7df80751e92ca39a6dc08a24d9fe170fe
mohd-Jainuddin <zainuddinshaikh3032@gmail.com>
2024-01-15 18:37:56 +0530

    Donation Form

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     document.getElementById("donationPopup").style.display = "block";
-  }, 2000); // Show popup after 2 seconds (adjust as needed)
+  }, 1000); // Show popup after 1 seconds (adjust as needed)
 });
 
 function closePopup() {
   document.getElementById("donationPopup").style.display = "none";
 }
 
-document.getElementById("btn").addEventListener("click", () => {
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let number = document.getElementById("number").value;
-  let amount = document.getElementById("amount").value;
 
-  if ((name != "" && email != "", number != "", amount != "")) {
-    alert(`Thank You ${name}`);
-  }
-});
+
+
