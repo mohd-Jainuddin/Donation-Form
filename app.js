89f8627d471d95086dca074fb66173f9a75a23a6
mohd-Jainuddin <zainuddinshaikh3032@gmail.com>
2024-01-16 08:53:41 +0530

    Donation Form

--- app.js
+++ app.js
@@ -10,9 +10,19 @@ function closePopup() {
 
 let button = document.getElementById("btn");
 
-button.addEventListener("click",()=>{
+button.addEventListener("click", () => {
   let name = document.getElementById("name").value;
-  alert(`Thank You ${name}`)
-})
+  let amount = document.getElementById("amount").value;
+  let email = document.getElementById("email").value;
+  let number = document.getElementById("number").value;
 
-
+  if (name != "" && amount != "" && email != "" && number != "") {
+    // Check if the input is a 10-digit number
+    if (/^\d{10}$/.test(number)) {
+      // Form is valid, you can submit it or perform further actions
+      alert(
+        "Thank you for your donation! Your generosity is making a positive impact. We appreciate your support and commitment to our cause. Together, we can make a difference!"
+      );
+    }
+  }
+});
